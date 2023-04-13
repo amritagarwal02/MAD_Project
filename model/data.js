577f16ae81ed701486cd6ddd8843e078d401c83c
amritagarwal02 <115497262+amritagarwal02@users.noreply.github.com>
2023-04-13 09:07:46 +0530

    Add files via upload

---
+++ model/data.js
@@ -0,0 +1,44 @@
+const songs =[
+    {
+        id:1,
+        title:"Ishq Vala Love",
+        artist:"Neeti Mohan",
+        artwork:require('../assets/img/ishq_vala_love.jpg'),
+        url:'https://drive.google.com/file/d/1nXJthzGNYhUML2xL0pUYL2IfJEXsDTCU/view?usp=share_link'
+
+    },
+    {
+        id:2,
+        title:"Dariya",
+        artist:"Arko",
+        artwork:require('../assets/img/dariya.jpg'),
+        url:'https://drive.google.com/file/d/1dntDOjqRQxbH5KOO0Qw4g5O32ki1X5By/view?usp=share_link'
+
+    },
+    {
+        id:3,
+        title:"Zaalima",
+        artist:"Arijit Singh",
+        artwork:require('../assets/img/zaalima.jpg'),
+        url:'https://drive.google.com/file/d/1dntDOjqRQxbH5KOO0Qw4g5O32ki1X5By/view?usp=share_link'
+
+    },
+    {
+        id:4,
+        title:"Tujhe kitna chahen aur",
+        artist:"Jubin Nautiyal",
+        artwork:require('../assets/img/tujhe_kitna.jpg'),
+        url:'https://drive.google.com/file/d/1kAMyNJzrBihfZIrol6_mgOS6N92XyjCr/view?usp=share_link'
+
+    },
+    {
+        id:5,
+        title:"Sab Tera",
+        artist:"Armaan Malik",
+        artwork:require('../assets/img/sab_tera.jpg'),
+        url:'https://drive.google.com/file/d/1dyaNyNI58_6thA2BJQya3UTgKjFJeAza/view?usp=share_link'
+
+    }
+]
+
+export default songs;
